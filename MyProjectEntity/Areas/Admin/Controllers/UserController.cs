9b9431f7ac2c6c008ebfd59fa705eda74df0070c
mediga vamshi <mediga-vamshi@priyanet.com>
2025-03-31 18:41:24 +0530

    crud

--- MyProjectEntity/Areas/Admin/Controllers/UserController.cs
+++ MyProjectEntity/Areas/Admin/Controllers/UserController.cs
@@ -4,63 +4,73 @@ using Entities;
 using EntitiesViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MyProjectEntity.Areas.Admin.Controllers
 {
-
     [Area("Admin")]
     public class UserController : Controller
     {
-        private readonly IUsers _users;
+        private readonly ServiceLayer<UserDomainModel> _userService;
         private readonly IMapper _mapper;
-        public UserController(IUsers users,IMapper mapper) {
-        
-            _users = users;
-            _mapper = mapper;
-        }
-        // GET: UserController
-        public async Task<ActionResult> Index()
+        private readonly ApiService _apiService;
+
+        public UserController(ServiceLayer<UserDomainModel> userService, IMapper mapper, ApiService apiService)
         {
-            List<UserDomainModel> users = await _users.GetUsers();
+            _userService = userService;
+            _mapper = mapper;
+            _apiService = apiService;
+        }
+
+        // GET: UserController
+        public async Task<IActionResult> Index()
+        {
+            List<UserDomainModel> users = await _userService.GetAllAsync(_apiService.GetApiUrl(ApiEndpoint.User));
             List<UserViewModel> usersViewModel = _mapper.Map<List<UserViewModel>>(users);
             return View(usersViewModel);
         }
 
         // GET: UserController/Details/5
-        public async Task<ActionResult> Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
-            UserDomainModel userDomainModel =await _users.GetUsersByid(id);
+            UserDomainModel userDomainModel = await _userService.GetByIdAsync(_apiService.GetApiUrl(ApiEndpoint.User), id);
             UserViewModel userViewModel = _mapper.Map<UserViewModel>(userDomainModel);
             return View(userViewModel);
         }
 
         // GET: UserController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
-            UserViewModel userDomainModel = new UserViewModel();
-            return View(userDomainModel);
+            return View(new UserViewModel());
         }
 
         // POST: UserController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(UserDomainModel userDomainModel)
+        public async Task<IActionResult> Create(UserViewModel userViewModel)
         {
             try
             {
-                int id = await _users.insert(userDomainModel);
-                return RedirectToAction(nameof(Index));
+                UserDomainModel userDomainModel = _mapper.Map<UserDomainModel>(userViewModel);
+                int id = await _userService.AddAsync(_apiService.GetApiUrl(ApiEndpoint.User), userDomainModel);
+                if (id > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", "Failed to create user.");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", "Error creating user: " + ex.Message);
             }
+            return View(userViewModel);
         }
 
         // GET: UserController/Edit/5
-        public async Task<ActionResult> Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
-            UserDomainModel userDomainModel = await _users.GetUsersByid(id);
+            UserDomainModel userDomainModel = await _userService.GetByIdAsync(_apiService.GetApiUrl(ApiEndpoint.User), id);
             UserViewModel userViewModel = _mapper.Map<UserViewModel>(userDomainModel);
             return View(userViewModel);
         }
@@ -68,24 +78,29 @@ namespace MyProjectEntity.Areas.Admin.Controllers
         // POST: UserController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(int id, UserViewModel userViewModel)
+        public async Task<IActionResult> Edit(int id, UserViewModel userViewModel)
         {
             try
             {
-                UserDomainModel user = _mapper.Map<UserDomainModel>(userViewModel);
-                bool b = await _users.update(user);
-                return RedirectToAction(nameof(Index));
+                UserDomainModel userDomainModel = _mapper.Map<UserDomainModel>(userViewModel);
+                bool isSuccess = await _userService.UpdateAsync($"{_apiService.GetApiUrl(ApiEndpoint.User)}/{id}", userDomainModel);
+                if (isSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", "Failed to update user.");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", "Error updating user: " + ex.Message);
             }
+            return View(userViewModel);
         }
 
         // GET: UserController/Delete/5
-        public async Task<ActionResult> Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            UserDomainModel userDomainModel = await _users.GetUsersByid(id);
+            UserDomainModel userDomainModel = await _userService.GetByIdAsync(_apiService.GetApiUrl(ApiEndpoint.User), id);
             UserViewModel userViewModel = _mapper.Map<UserViewModel>(userDomainModel);
             return View(userViewModel);
         }
@@ -93,17 +108,22 @@ namespace MyProjectEntity.Areas.Admin.Controllers
         // POST: UserController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
             try
             {
-                int b = await _users.delete(id);
-                return RedirectToAction(nameof(Index));
+                bool isSuccess = await _userService.DeleteAsync(_apiService.GetApiUrl(ApiEndpoint.User), id);
+                if (isSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", "Failed to delete user.");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", "Error deleting user: " + ex.Message);
             }
+            return RedirectToAction(nameof(Delete), new { id });
         }
     }
 }
