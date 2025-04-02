ccd9fc22a21432f78460b6bc312ed26b3cece95e
mediga vamshi <mediga-vamshi@priyanet.com>
2025-04-02 18:03:17 +0530

    login

---
+++ CourseViewModel/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesViewModel
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [Display(Name = "Username")] // Custom label for the form
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+}
