a56f04008b12b76f3123d1b3b76b98dd91f8b28f
mediga vamshi <mediga-vamshi@priyanet.com>
2025-04-16 16:34:36 +0530

    crud

---
+++ BusinessLayer/interface/ISystemAdmin.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface ISystemAdmin
+    {
+        Task<List<SystemAdminDomainModel>> GetAllAsync();
+        Task<SystemAdminDomainModel> GetByIdAsync(int systemAdminId);
+        Task<int> InsertAsync(SystemAdminDomainModel admin);
+        Task<bool> UpdateAsync(SystemAdminDomainModel admin);
+        Task<bool> DeleteAsync(int systemAdminId);
+    }
+}
